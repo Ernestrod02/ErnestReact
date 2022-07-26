f5a5374bf481125b34fc572d78c654ba9be0d102
ernest <ernestrod02@gmail.com>
2022-07-25 20:48:51 -0500

    Fixed the navbar logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Donuts & &nbsp;
-            <i class="fa-solid fa-code"></i>
+            Donuts & &nbsp; <i class="fa-solid fa-code"></i>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
